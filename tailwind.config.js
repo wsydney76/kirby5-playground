14fdfbc4f1b01787011248f6df362c9be135ec68
wsydn <wsydney@web.de>
2025-06-24 19:28:43 +0200

    prepare for Tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors')
-
 export default {
   content: [
     './templates/**/*.twig',
@@ -12,26 +10,11 @@ export default {
       screens: {
         xl: '1200px',
       },
-      colors: {
-        // set a brand color
-        brand: colors.slate,
 
-        // set the global gray flavor
-        gray: colors.slate,
-
-        // let colors match the text of the text color of the typography plugin
-        prose: colors.slate[900]
-      },
 
       // make the list bullets a bit more visible
       // https://tailwindcss.com/docs/typography-plugin#adding-custom-color-themes
-      typography: (theme) => ({
-        slate: {
-          css: {
-            '--tw-prose-bullets': theme('colors.gray[400]'),
-          }
-        }
-      }),
+
     },
     transitionDuration: {
       DEFAULT: '400ms',
